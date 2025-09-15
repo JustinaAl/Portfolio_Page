f534d3675c65d4a1a3c3ecf5e1e955536c1b7d30
JustinaAl <juste.paskeviciute@gmail.com>
2025-09-15 10:39:22 +0200

    .

--- src/assets/lists/travelList.js
+++ src/assets/lists/travelList.js
@@ -1,13 +1,13 @@
 export const travelList = [
-        '../assets/keliones_SQ/DSC00688.jpg',
-        './assets/keliones_SQ/DSC00745.jpg',
-        './public/assets/keliones_SQ/IMG_0827.jpg',
+        '/assets/keliones_SQ/DSC00688.jpg',
+        '/assets/keliones_SQ/DSC00745.jpg',
+        '/assets/keliones_SQ/IMG_0827.jpg',
 
-        '../public/assets/keliones_SQ/DSC05290.jpg',
-        'src/assets/keliones_SQ/DSC07858.jpg',
-        'src/assets/keliones_SQ/DSC05338.jpg',
+        '/assets/keliones_SQ/DSC05290.jpg',
+        '/assets/keliones_SQ/DSC07858.jpg',
+        '/assets/keliones_SQ/DSC05338.jpg',
 
-        'src/assets/keliones_SQ/mafor.jpg',
-        'src/assets/keliones_SQ/IMG_2769.jpg',
-        'src/assets/keliones_SQ/DSC02419.jpg',
+        '/assets/keliones_SQ/mafor.jpg',
+        '/assets/keliones_SQ/IMG_2769.jpg',
+        '/assets/keliones_SQ/DSC02419.jpg',
     ]
